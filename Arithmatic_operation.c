7df912daac9586170768cfb53c668b0841966cdd
Monika Bade <monika.bade04@gmail.com>
2024-05-18 23:15:16 +0530

    Add files via upload
    all arithmatic operation perform in one program 
    c language 
    dev c++ platform

---
+++ Arithmatic_operation.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a = 28, b = 12;
+	printf("a+b = %d\n", a+b);
+	printf("a-b = %d\n", a-b);
+	printf("a*b = %d\n", a*b);
+	printf("a/b = %d\n", a/b);
+	printf("a % b = %d\n", a%b);
+	printf("+a = %d\n", +a);
+	printf("-a = %d\n", -a);
+	printf("++a = %d\n", ++a);
+	printf("a-- = %d\n", a--);
+	return 0;
+	
+}
